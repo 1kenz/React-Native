8cc335664f90f0d87921051c04839b9c760bead6
xkendx <63884610+xkendx@users.noreply.github.com>
2021-08-02 01:24:00 +0300

    feat: start recap

--- learning/React-Native-Lessons/index.js
+++ learning/React-Native-Lessons/index.js
@@ -3,7 +3,7 @@
  */
 import 'react-native-gesture-handler';
 import { AppRegistry } from 'react-native';
-import App from './__lesson__/session01/src/App.js';
+import App from './__lesson__/session00/src/ShopCard';
 import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
