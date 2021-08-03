c9053759fb5d67d17084c3deb2aa92cd394c3ddf
xkendx <63884610+xkendx@users.noreply.github.com>
2021-08-03 03:03:07 +0300

    feat: send props on functions

--- learning/firstProject/App.js
+++ learning/firstProject/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { View, Text, Button } from 'react-native';
 
 function App() {
-  const sayHello = () => {
-    console.log('Hello');
+  const sayHello = name => {
+    console.log('Hello ' + name);
   };
 
   return (
@@ -12,7 +12,7 @@ function App() {
         <Text>Hello World!!</Text>
         <Text>Hello New World!!</Text>
       </View>
-      <Button title="Click Me" onPress={sayHello} />
+      <Button title="Click Me" onPress={() => sayHello('Kenan')} />
     </View>
   );
 }
