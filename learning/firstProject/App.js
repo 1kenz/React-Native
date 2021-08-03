7c69e37027a9d4a6a9e2cab0d6704c0bceb50211
xkendx <63884610+xkendx@users.noreply.github.com>
2021-08-03 03:02:08 +0300

    feat: use onPress props

--- learning/firstProject/App.js
+++ learning/firstProject/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import { View, Text, Button } from 'react-native';
 
 function App() {
+  const sayHello = () => {
+    console.log('Hello');
+  };
+
   return (
     <View>
       <View style={{ backgroundColor: 'red' }}>
         <Text>Hello World!!</Text>
         <Text>Hello New World!!</Text>
       </View>
+      <Button title="Click Me" onPress={sayHello} />
     </View>
   );
 }
