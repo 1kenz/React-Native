05edfb33ed0e208f0a06910233e9485c4dad90d5
xkendx <63884610+xkendx@users.noreply.github.com>
2021-08-03 03:53:13 +0300

    feat: flex advance

--- learning/firstProject/App.js
+++ learning/firstProject/App.js
@@ -4,12 +4,17 @@ import { View, Text, StyleSheet } from 'react-native';
 function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.upper_view_container}>
-        <Text>Hello World!!</Text>
+      <View style={styles.box1}>
+        <Text>Box-1</Text>
       </View>
-
-      <View style={styles.bottom_view_container}>
-        <Text>Hi World!!</Text>
+      <View style={styles.box2}>
+        <Text>Box-2</Text>
+      </View>
+      <View style={styles.box3}>
+        <Text>Box-3</Text>
+      </View>
+      <View style={styles.box4}>
+        <Text>Box-4</Text>
       </View>
     </View>
   );
@@ -18,17 +23,36 @@ function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // flexDirection: 'row',
+    flexDirection: 'column',
+    backgroundColor: 'gray',
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
-  upper_view_container: {
-    flex: 1,
+  box1: {
+    width: 100,
+    height: 100,
     backgroundColor: 'yellow',
     justifyContent: 'center',
     alignItems: 'center',
   },
-  bottom_view_container: {
-    flex: 10,
+  box2: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'blue',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  box3: {
+    width: 100,
+    height: 100,
     backgroundColor: 'red',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  box4: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'green',
     justifyContent: 'center',
     alignItems: 'center',
   },
